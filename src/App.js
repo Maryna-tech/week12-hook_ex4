1ec56a0955a212892fcf31f831adb1d122abace8
Maryna-tech <mgaraeva41@gmail.com>
2024-12-24 16:30:39 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useEffect, useReducer, useRef } from 'react';
+import './App.css';
+
+function reducer( state,action){
+  switch (action.type){
+    case'START':return {...state, isTicking:true};
+    case'PAUSE':return {...state, isTicking:false};
+    case 'RESET':return {clock:0, isTicking:false};
+    case'TICK':return {... state, clock: state.clock +1};
+  }
+}
+
+  const initialState = {
+    clock:0,
+    isTicking:false
+}
+
+function App() { 
+  const [state, dispatch] = useReducer( reducer, initialState);
+  const myTimerRef = useRef (0);
+
+  useEffect (()=> {
+    if (!state.isTicking){
+      return;
+    }
+      myTimerRef.current = setInterval (()=> dispatch ({ type:'TICK'}), 1000);
+      
+      return()=>{
+        clearInterval(myTimerRef.current)
+        myTimerRef.current = 0
+  }
+  }, [state.isTicking])
+
+  return (
+    <div className="App">
+      <p>{state.clock} seconds</p>
+      <button onClick={() => dispatch ({type:'START'})}>START</button>
+      <button onClick={() => dispatch ({type:'PAUSE'})}>PAUSE</button>
+      <button onClick={() => dispatch ({type:'RESET'})}>RESET</button>
+    </div>
+  );
+}
+
+export default App;
